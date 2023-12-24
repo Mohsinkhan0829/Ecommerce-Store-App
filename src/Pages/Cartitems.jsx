65a3d39312ad0b6b63bc387fc5447fc9a75690f4
unknown <research.mohsinkhan@gmail.com>
2023-12-24 16:28:19 +0530

    add functionality in cart

--- src/Pages/Cartitems.jsx
+++ src/Pages/Cartitems.jsx
@@ -1,8 +1,11 @@
 import React, { useContext } from "react";
 import { AllProducts } from "../Components/Context/ProductContext";
 
-const Cartitems = () => {
-  const { AllProductsData, cartItem, removeFromCart } = useContext(AllProducts);
+const Cartitems = (Props) => {
+  const { Product } = Props;
+
+  const { AllProductsData, cartItem, removeFromCart, addToCart } =
+    useContext(AllProducts);
   return (
     <div className="CartItems">
       <div className="cartItem-main">
@@ -14,23 +17,41 @@ const Cartitems = () => {
         <p>Remove</p>
       </div>
       <hr />
-    {AllProductsData.map((e)=>{
-        if(cartItem[e.id]>0) 
-        {
-            return <div>
-            <div className='cartItem-format cartItem-main'>
-            <img src={e.image} alt="" />
-            <p>{e.name}</p>
-            <p>${e.Price}</p>
-            <button className="cartItem-Quantity">{cartItem[e.id]}</button>
-            <p>${e.Price*cartItem[e.id]}</p>
-            <button className="RemoveButton" onClick={()=>{removeFromCart(e.id)} }>Remove</button>
-          </div>
-          <hr />
-          </div>
+      {AllProductsData.map((e) => {
+        if (cartItem[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartItem-format cartItem-main">
+                <img src={e.image} alt="" />
+                <p>{e.name}</p>
+                <p>${e.Price}</p>
+                <button className="cartItem-Quantity">
+                  {cartItem[e.id]}
+                  <button
+                    className="addProduct"
+                    onClick={() => {
+                      addToCart(e.id);
+                    }}
+                  >
+                    Add
+                  </button>
+                </button>
+                <p>${e.Price * cartItem[e.id]}</p>
+                <button
+                  className="RemoveButton"
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+              <hr />
+            </div>
+          );
         }
-        return null
-    })}
+        return null;
+      })}
     </div>
   );
 };
